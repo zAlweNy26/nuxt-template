9c456471b2f499fe3cb3cc4fc21687edb5ad3cb5
Dany <zdanymc@gmail.com>
2024-07-28 23:24:30 +0200

    feat(Component): Add Skeleton

---
+++ components/Skeleton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div :class="cn('animate-pulse rounded-md bg-primary/10', props.class)" />
+</template>
