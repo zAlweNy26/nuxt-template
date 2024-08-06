92392690181ca440842f79c84d9987e880dc8ac1
Dany <zdanymc@gmail.com>
2024-08-06 21:47:20 +0200

    fix(Component): Update Paginator

--- components/Paginator.vue
+++ components/Paginator.vue
@@ -3,7 +3,7 @@ import { PaginationEllipsis, PaginationRoot, PaginationList, PaginationListItem,
   PaginationFirst, PaginationPrev, PaginationNext, PaginationLast, type PaginationRootProps } from 'radix-vue'
 import type { ButtonProps } from './ui/button'
 
-withDefaults(defineProps<Omit<PaginationRootProps, 'as' | 'asChild' | 'page'> & { size: ButtonProps['size'] }>(), {
+withDefaults(defineProps<Omit<PaginationRootProps, 'as' | 'asChild' | 'page'> & { size?: ButtonProps['size'] }>(), {
   disabled: false,
   defaultPage: 1,
   siblingCount: 1,
@@ -12,6 +12,14 @@ withDefaults(defineProps<Omit<PaginationRootProps, 'as' | 'asChild' | 'page'> & 
   size: 'sm'
 })
 
+defineEmits<{
+  first: []
+  prev: []
+  next: []
+  last: []
+  update: [value: number]
+}>()
+
 const model = defineModel<number>({ default: 1 })
 </script>
 
@@ -19,19 +27,19 @@ const model = defineModel<number>({ default: 1 })
   <PaginationRoot v-slot="{ page }" v-model:page="model" v-bind="$props">
     <PaginationList v-slot="{ items }" class="flex items-center gap-1">
       <PaginationFirst as-child>
-        <Button square variant="outline" :size>
+        <Button square variant="outline" :size @click="$emit('first')">
           <Icon name="ph:caret-double-left-bold" class="size-4" />
         </Button>
       </PaginationFirst>
       <PaginationPrev as-child>
-        <Button square variant="outline" :size>
+        <Button square variant="outline" :size @click="$emit('prev')">
           <Icon name="ph:caret-left-bold" class="size-4" />
         </Button>
       </PaginationPrev>
       <template v-for="(item, index) in items">
         <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
-          <slot :selected="item.value === page">
-            <Button square :size :variant="item.value === page ? 'primary' : 'outline'">
+          <slot :page="item.value" :selected="item.value === page" :click="() => $emit('update', item.value)">
+            <Button square :size :variant="item.value === page ? 'primary' : 'outline'" @click="$emit('update', item.value)">
               {{ item.value }}
             </Button>
           </slot>
@@ -43,12 +51,12 @@ const model = defineModel<number>({ default: 1 })
         </PaginationEllipsis>
       </template>
       <PaginationNext as-child>
-        <Button square variant="outline" :size>
+        <Button square variant="outline" :size @click="$emit('next')">
           <Icon name="ph:caret-right-bold" class="size-4" />
         </Button>
       </PaginationNext>
       <PaginationLast as-child>
-        <Button square variant="outline" :size>
+        <Button square variant="outline" :size @click="$emit('last')">
           <Icon name="ph:caret-double-right-bold" class="size-4" />
         </Button>
       </PaginationLast>
