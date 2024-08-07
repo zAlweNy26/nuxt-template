30a97e7b7db3cf5e0b7d244b378c76ae482aa139
Dany <zdanymc@gmail.com>
2024-08-07 20:39:12 +0200

    fix(Component): Update Paginator

--- components/Paginator.vue
+++ components/Paginator.vue
@@ -3,12 +3,17 @@ import { PaginationEllipsis, PaginationRoot, PaginationList, PaginationListItem,
   PaginationFirst, PaginationPrev, PaginationNext, PaginationLast, type PaginationRootProps } from 'radix-vue'
 import type { ButtonProps } from './ui/button'
 
-withDefaults(defineProps<Omit<PaginationRootProps, 'as' | 'asChild' | 'page'> & { size?: ButtonProps['size'] }>(), {
+const props = withDefaults(defineProps<Omit<PaginationRootProps, 'as' | 'asChild' | 'page'> & { 
+  size?: ButtonProps['size'], 
+  showText?: boolean,
+  total: number 
+}>(), {
   disabled: false,
   defaultPage: 1,
   siblingCount: 1,
   showEdges: true,
   itemsPerPage: 10,
+  showText: false,
   size: 'sm'
 })
 
@@ -21,45 +26,58 @@ defineEmits<{
 }>()
 
 const model = defineModel<number>({ default: 1 })
+
+const pageFrom = computed(() => (model.value - 1) * props.itemsPerPage + 1)
+const pageTo = computed(() => Math.min(model.value * props.itemsPerPage, props.total))
 </script>
 
 <template>
-  <PaginationRoot v-slot="{ page }" v-model:page="model" v-bind="$props">
-    <PaginationList v-slot="{ items }" class="flex items-center gap-1">
-      <PaginationFirst as-child>
-        <Button square variant="outline" :size @click="$emit('first')">
-          <Icon name="ph:caret-double-left-bold" class="size-4" />
-        </Button>
-      </PaginationFirst>
-      <PaginationPrev as-child>
-        <Button square variant="outline" :size @click="$emit('prev')">
-          <Icon name="ph:caret-left-bold" class="size-4" />
-        </Button>
-      </PaginationPrev>
-      <template v-for="(item, index) in items">
-        <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
-          <slot :page="item.value" :selected="item.value === page" :click="() => $emit('update', item.value)">
-            <Button square :size :variant="item.value === page ? 'primary' : 'outline'" @click="$emit('update', item.value)">
-              {{ item.value }}
+  <div class="flex w-full flex-col gap-2">
+    <slot :page="model" />
+    <div class="flex items-center justify-between gap-2">
+      <slot v-if="showText" name="text" :page="model" :pageFrom :pageTo>
+        <i18n-t keypath="table.pagination" tag="p" class="text-sm leading-5">
+          <span class="font-semibold">{{ pageFrom }}</span>
+          <span class="font-semibold">{{ pageTo }}</span>
+          <span class="font-semibold">{{ total }}</span>
+        </i18n-t>
+      </slot>
+      <PaginationRoot v-slot="{ page }" v-model:page="model" v-bind="$props">
+        <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+          <PaginationFirst as-child>
+            <Button square variant="outline" :size @click="$emit('first')">
+              <Icon name="ph:caret-double-left-bold" class="size-4" />
             </Button>
-          </slot>
-        </PaginationListItem>
-        <PaginationEllipsis v-else :key="item.type" :index="index" class="grid size-8 place-content-center">
-          <slot name="ellipsis">
-            <Icon name="ph:dots-three-bold" class="size-6" />
-          </slot>
-        </PaginationEllipsis>
-      </template>
-      <PaginationNext as-child>
-        <Button square variant="outline" :size @click="$emit('next')">
-          <Icon name="ph:caret-right-bold" class="size-4" />
-        </Button>
-      </PaginationNext>
-      <PaginationLast as-child>
-        <Button square variant="outline" :size @click="$emit('last')">
-          <Icon name="ph:caret-double-right-bold" class="size-4" />
-        </Button>
-      </PaginationLast>
-    </PaginationList>
-  </PaginationRoot>
+          </PaginationFirst>
+          <PaginationPrev as-child>
+            <Button square variant="outline" :size @click="$emit('prev')">
+              <Icon name="ph:caret-left-bold" class="size-4" />
+            </Button>
+          </PaginationPrev>
+          <template v-for="(item, index) in items">
+            <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+              <Button square :size :variant="item.value === page ? 'primary' : 'outline'" @click="$emit('update', item.value)">
+                {{ item.value }}
+              </Button>
+            </PaginationListItem>
+            <PaginationEllipsis v-else :key="item.type" :index="index" class="grid size-8 place-content-center">
+              <slot name="ellipsis">
+                <Icon name="ph:dots-three-bold" class="size-6" />
+              </slot>
+            </PaginationEllipsis>
+          </template>
+          <PaginationNext as-child>
+            <Button square variant="outline" :size @click="$emit('next')">
+              <Icon name="ph:caret-right-bold" class="size-4" />
+            </Button>
+          </PaginationNext>
+          <PaginationLast as-child>
+            <Button square variant="outline" :size @click="$emit('last')">
+              <Icon name="ph:caret-double-right-bold" class="size-4" />
+            </Button>
+          </PaginationLast>
+        </PaginationList>
+      </PaginationRoot>
+    </div>
+  </div>
 </template>
