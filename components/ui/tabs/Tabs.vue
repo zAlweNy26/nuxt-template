4cb173443910994466f572620d91fbcd933feb35
Dany <zdanymc@gmail.com>
2024-08-16 13:42:36 +0200

    style(Component): Update Tabs css

--- components/ui/tabs/Tabs.vue
+++ components/ui/tabs/Tabs.vue
@@ -22,8 +22,8 @@ onMounted(() => {
 <template>
   <TabsRoot v-model="model" v-bind="props.root" :class="cn('flex gap-2', { 'flex-col': props.root?.orientation === 'horizontal' })">
     <TabsList :loop :class="cn(
-      'inline-flex items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground',
-      props.root?.orientation === 'vertical' ? 'flex-col' : 'h-9',
+      'inline-flex items-center justify-center transition-colors gap-1 rounded-lg bg-muted p-1 text-muted-foreground',
+      props.root?.orientation === 'vertical' ? 'flex-col' : '',
     )">
       <TabsTrigger v-for="item in items" :key="item.id" :value="item.id" :disabled="item.disabled" :class="cn(
         'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
