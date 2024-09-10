1a1e6a8d952768b80c74677b6578d1907787bbfd
Dany <zdanymc@gmail.com>
2024-09-10 13:44:52 +0200

    feat(Component): Add indicator animation in Tabs

--- components/ui/tabs/Tabs.vue
+++ components/ui/tabs/Tabs.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { TabsRoot, TabsTrigger, TabsList, TabsContent, type TabsRootProps } from 'radix-vue'
+import { TabsRoot, TabsTrigger, TabsList, TabsContent, TabsIndicator, type TabsRootProps } from 'radix-vue'
 import type { TabItems } from '.'
 import type { ClassValue } from 'clsx'
 
@@ -22,11 +22,18 @@ onMounted(() => {
 <template>
   <TabsRoot v-model="model" v-bind="props.root" :class="cn('flex gap-2', { 'flex-col': props.root?.orientation === 'horizontal' })">
     <TabsList :loop :class="cn(
-      'inline-flex items-center justify-center transition-colors gap-1 rounded-lg bg-muted p-1 text-muted-foreground',
+      'inline-flex relative items-center justify-center transition-colors gap-1 rounded-lg bg-muted p-1 text-muted-foreground',
       props.root?.orientation === 'vertical' ? 'flex-col' : '',
     )">
+      <TabsIndicator :class="cn(
+        'absolute transition-[width,transform] duration-200', props.root?.orientation === 'vertical' ? 
+        'w-full left-0 top-0 px-1 h-[--radix-tabs-indicator-size] translate-y-[--radix-tabs-indicator-position]' : 
+        'h-full translate-x-[--radix-tabs-indicator-position] py-1 bottom-0 left-0 w-[--radix-tabs-indicator-size]',
+      )">
+        <div class="size-full rounded-md bg-background shadow" />
+      </TabsIndicator>
       <TabsTrigger v-for="item in items" :key="item.id" :value="item.id" :disabled="item.disabled" :class="cn(
-        'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
+        'inline-flex z-10 items-center justify-center data-[state=active]:text-foreground whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
         props.root?.orientation === 'vertical' ? 'w-full' : '',
         item.class,
       )">
