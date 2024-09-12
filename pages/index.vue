e892da55337f75cb2a7809eb4fde27af2425752e
Dany <zdanymc@gmail.com>
2024-09-12 18:19:36 +0200

    feat(Component): Add Resizable

--- pages/index.vue
+++ pages/index.vue
@@ -446,5 +446,28 @@ onMounted(async () => {
         <p>{{ item.description }}</p>
       </div>
     </Carousel>
+    <ResizableGroup direction="horizontal" class="max-w-md rounded-lg border">
+      <ResizablePanel :defaultSize="50">
+        <div class="flex h-[200px] items-center justify-center p-6">
+          <span class="font-semibold">One</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel :defaultSize="50">
+        <ResizableGroup direction="vertical">
+          <ResizablePanel :defaultSize="25">
+            <div class="flex h-full items-center justify-center p-6">
+              <span class="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel :defaultSize="75">
+            <div class="flex h-full items-center justify-center p-6">
+              <span class="font-semibold">Three</span>
+            </div>
+          </ResizablePanel>
+        </ResizableGroup>
+      </ResizablePanel>
+    </ResizableGroup>
   </div>
 </template>
