7c874c968a935234629213e89805054099e95c21
Dany <zdanymc@gmail.com>
2024-09-21 00:26:22 +0200

    feat!: Add tv to Accordion

--- components/ui/accordion/Accordion.vue
+++ components/ui/accordion/Accordion.vue
@@ -1,48 +1,38 @@
 <script setup lang="ts">
-import { useForwardPropsEmits, AccordionRoot, AccordionItem, AccordionHeader, AccordionTrigger,
-  AccordionContent, type AccordionRootEmits, type AccordionRootProps } from 'radix-vue'
-import type { AccordionItems } from '.'
+import { Accordion } from 'radix-vue/namespaced'
+import { useForwardPropsEmits, type AccordionRootEmits } from 'radix-vue'
+import { type AccordionProps, accordionVariants } from '.'
 
-const props = withDefaults(defineProps<AccordionRootProps & {
-  items: AccordionItems
-  class?: ClassValue
-  contentClass?: ClassValue
-}>(), {
+const props = withDefaults(defineProps<AccordionProps>(), {
   type: 'single',
-  class: undefined,
-  contentClass: undefined,
   collapsible: true
 })
 
+const rootProps = reactivePick(props, (v, k) => !['items', 'class', 'ui'].includes(k))
+
+const { trigger, icon, content, item: element } = accordionVariants()
+
 const emits = defineEmits<AccordionRootEmits>()
 
-const delegatedProps = computed(() => {
-  const { items: _, class: __, contentClass: ___, ...delegated } = props
-  return delegated
-})
-
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
+const forwarded = useForwardPropsEmits(rootProps, emits)
 </script>
 
 <template>
-  <AccordionRoot v-bind="forwarded" :class="cn('w-full divide-y', props.class)">
-    <AccordionItem v-for="item in items" :key="item.id" :value="item.id" :class="cn('pb-2', contentClass)">
-      <AccordionHeader class="flex">
-        <AccordionTrigger :class="cn(
-          'flex flex-1 gap-1 w-full items-center justify-between py-2 text-sm font-medium transition-all cursor-pointer [&[data-state=open]>.accordion-icon]:rotate-180',
-          item.class,
-        )">
+  <Accordion.Root v-bind="forwarded" :class="cn('w-full divide-y', props.class)">
+    <Accordion.Item v-for="item in items" :key="item.id" :value="item.id" :class="cn(element(), ui?.item, item.class)">
+      <Accordion.Header asChild>
+        <Accordion.Trigger :class="cn(trigger(), ui?.trigger)">
           <slot :item>
             <span>{{ item.label }}</span>
           </slot>
           <slot name="icon">
-            <Icon name="ph:caret-down-bold" class="accordion-icon size-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+            <Icon name="ph:caret-down-bold" :class="cn(icon(), ui?.icon)" />
           </slot>
-        </AccordionTrigger>
-      </AccordionHeader>
-      <AccordionContent class="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
+        </Accordion.Trigger>
+      </Accordion.Header>
+      <Accordion.Content :class="cn(content(), ui?.content)">
         <slot :name="`${item.id}-content`" />
-      </AccordionContent>
-    </AccordionItem>
-  </AccordionRoot>
+      </Accordion.Content>
+    </Accordion.Item>
+  </Accordion.Root>
 </template>
--- components/ui/accordion/index.ts
+++ components/ui/accordion/index.ts
@@ -1,3 +1,5 @@
+import type { AccordionRootProps } from 'radix-vue'
+
 export { default as Accordion } from './Accordion.vue'
 
 export type AccordionItem = {
@@ -8,3 +10,20 @@ export type AccordionItem = {
 }
 
 export type AccordionItems = [AccordionItem, ...AccordionItem[]]
+
+export const accordionVariants = compose({
+    slots: {
+        trigger: 'flex flex-1 gap-1 w-full items-center justify-between py-2 text-sm font-medium transition-all cursor-pointer [&[data-state=open]>.accordion-icon]:rotate-180',
+        content: 'overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
+        item: 'pb-2',
+        icon: 'accordion-icon size-4 shrink-0 text-muted-foreground transition-transform duration-200'
+    },
+})
+
+export type AccordionProps = AccordionRootProps & { 
+    items: AccordionItems
+    class?: ClassValue
+    ui?: {
+        [K in keyof typeof accordionVariants.slots]?: ClassValue
+    }
+}
