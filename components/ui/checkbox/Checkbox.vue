e132bc6b88aed99991c204c2a22b299923fec856
Dany <zdanymc@gmail.com>
2024-09-25 18:05:00 +0200

    feat(Checkbox)!: Add indeterminate value and improve props

--- components/ui/checkbox/Checkbox.vue
+++ components/ui/checkbox/Checkbox.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { CheckboxIndicator, CheckboxRoot, type CheckboxRootEmits, useForwardPropsEmits } from 'radix-vue'
-import { type CheckboxProps, checkboxVariants } from '.'
+import { CheckboxIndicator, CheckboxRoot, type CheckboxRootEmits, type CheckboxRootProps, useForwardPropsEmits } from 'radix-vue'
+import { type CheckboxVariants, checkboxVariants } from '.'
 
-const props = defineProps<CheckboxProps & { id?: string }>()
+const props = defineProps<CheckboxRootProps & {
+	variant?: CheckboxVariants['variant']
+	size?: CheckboxVariants['size']
+	class?: ClassValue
+	labelClass?: ClassValue
+	text?: string
+}>()
 const emits = defineEmits<CheckboxRootEmits>()
+
+const delegatedProps = reactiveOmit(props, 'variant', 'size', 'class', 'labelClass', 'text', 'id')
 
 const cbId = computed(() => props.id ?? `cb-${useId()}`)
 
-const forwarded = useForwardPropsEmits(() => props.root ?? {}, emits)
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
 	<div class="flex items-center space-x-2">
 		<CheckboxRoot :id="cbId" v-bind="forwarded" :class="cn(checkboxVariants({ variant, size }), props.class)">
 			<CheckboxIndicator class="flex size-full items-center justify-center text-current">
-				<slot>
-					<Icon name="ph:check-bold" class="size-4" />
+				<slot :checked>
+					<Icon v-if="checked === 'indeterminate'" name="ph:minus-bold" class="size-4" />
+					<Icon v-else name="ph:check-bold" class="size-4" />
 				</slot>
 			</CheckboxIndicator>
 		</CheckboxRoot>
--- components/ui/checkbox/index.ts
+++ components/ui/checkbox/index.ts
@@ -1,27 +1,17 @@
-import type { CheckboxRootProps } from 'radix-vue'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export { default as Checkbox } from './Checkbox.vue'
-
-export interface CheckboxProps {
-	variant?: CheckboxVariants['variant']
-	size?: CheckboxVariants['size']
-	root?: Omit<CheckboxRootProps, 'id'>
-	class?: ClassValue
-	labelClass?: ClassValue
-	text?: string
-}
 
 export const checkboxVariants = cva(
 	'peer size-4 shrink-0 rounded-sm shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
 	{
 		variants: {
 			variant: {
-				primary: 'border border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
-				error: 'border border-error data-[state=checked]:bg-error data-[state=checked]:text-error-foreground',
-				warning: 'border border-warning data-[state=checked]:bg-warning data-[state=checked]:text-warning-foreground',
-				success: 'border border-success data-[state=checked]:bg-success data-[state=checked]:text-success-foreground',
-				info: 'border border-info data-[state=checked]:bg-info data-[state=checked]:text-info-foreground',
+				primary: 'border border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=indeterminate]:text-primary',
+				error: 'border border-error data-[state=checked]:bg-error data-[state=checked]:text-error-foreground data-[state=indeterminate]:text-error',
+				warning: 'border border-warning data-[state=checked]:bg-warning data-[state=checked]:text-warning-foreground data-[state=indeterminate]:text-warning',
+				success: 'border border-success data-[state=checked]:bg-success data-[state=checked]:text-success-foreground data-[state=indeterminate]:text-success',
+				info: 'border border-info data-[state=checked]:bg-info data-[state=checked]:text-info-foreground data-[state=indeterminate]:text-info',
 			},
 			size: {
 				xs: 'size-3 rounded',
